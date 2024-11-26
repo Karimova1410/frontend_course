8a378c655c7291c6b0270ebde143abbc7a32e9ea
Роза Каримова <karimovaroza.2004@gmail.com>
2024-11-26 19:37:30 +0600

    initializing the data list on first render

--- data_filter/src/App.jsx
+++ data_filter/src/App.jsx
@@ -4,6 +4,28 @@ function App() {
 	const [data, setData] = useState([]);
 	const [searchTerm, setSearchTerm] = useState("");
 
+  useEffect(() => {
+		const fetchData = async () => {
+			const names = [
+				{ id: 1, name: "Roza" },
+				{ id: 2, name: "Aisuluu" },
+				{ id: 3, name: "Altynai" },
+				{ id: 4, name: "Zhanetta" },
+				{ id: 5, name: "Aleksandra" },
+				{ id: 6, name: "Dilnaz" },
+				{ id: 7, name: "Bermet" },
+				{ id: 8, name: "Aikan" },
+				{ id: 9, name: "Aijan eje" },
+				{ id: 10, name: "Zharkynai" },
+				{ id: 11, name: "Anzhelika" },
+			];
+			setData(names);
+		};
+
+		fetchData();
+	}, []);
+
+
 	return (
 		<div>
 			<h1>Data filtering</h1>
